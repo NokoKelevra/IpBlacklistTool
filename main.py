64fe6ae157bbdf68ba6e180b5b43f5a4a8041eec
Noko Kelevra <noko_kelevra@hotmail.com>
2026-02-02 16:52:27 +0100

    Añadimos la ejecucion de psad_blocker

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import subprocess
+
 from db.database import database_exists, create_database
 from config.settings import settings
+
+def run_blacklist_script():
+    """
+    Ejecuta el script psad_blocker.sh con sudo.
+    Devuelve stdout si todo va bien.
+    Lanza excepción si falla.
+    """
+    try:
+        result = subprocess.run(
+            ["sudo", settings.BLACKLIST_SCRIPT],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        return result.stdout
+
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(
+            f"Error ejecutando psad_blocker.sh\n"
+            f"STDOUT:\n{e.stdout}\n"
+            f"STDERR:\n{e.stderr}"
+        )
 
 def init_database():
     if not database_exists():
@@ -10,12 +35,19 @@ def init_database():
         print("[+] Base de datos encontrada.")
 
 def main():
-    print("Configuración correcta, seguimos...")
-    init_database()
-    # Aquí irá después:
-    # - ejecución del script bash
-    # - lectura de la blacklist
-    # - consulta a Shodan
+    print("[*] Iniciando aplicación")
+
+    # Validación y DB
+    if not database_exists():
+        print("[*] Base de datos no existe, creando...")
+        create_database()
+
+    print("[*] Ejecutando psad_blocker.sh...")
+    output = run_blacklist_script()
+
+    print("[+] Script ejecutado correctamente")
+    print("[*] Salida del script:")
+    print(output)
 
 
 if __name__ == "__main__":
